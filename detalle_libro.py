25fde4a13ef84e3036ba87727c4f0948b6f77af6
Seba Epulef <ale.epuin@gmail.com>
2024-09-23 11:01:05 -0300

    oe yapo

---
+++ detalle_libro.py
@@ -0,0 +1,5 @@
+class detalle_libro:
+    def __init__(self, categorias, nro_paginas, editorial):
+        self.categorias = categorias
+        self.nro_paginas = nro_paginas
+        self.editorial = editorial
