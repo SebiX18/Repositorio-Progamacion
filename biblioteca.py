b02507ab99ce70ff6c3c9e0150b24f50f999cda3
SebiX18 <ale.epuin@gmail.com>
2024-09-23 10:48:55 -0300

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,11 @@
+class biblioteca:
+    def __init__(self,nombre, direccion, telefono):
+        self.nombre = nombre
+        self.direcion = direccion
+        self.telefono = telefono
+    def buscar_libro(self):
+        pass
+    def prestar_libro(self):
+        pass
+    def devolver_libro(self):
+        pass
